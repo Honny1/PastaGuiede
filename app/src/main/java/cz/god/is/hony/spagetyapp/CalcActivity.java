855bf6dcd34225efcd3e3d80a13e42bd34cfcb08
Hony <hony.com@seznam.cz>
2017-05-22 15:53:06 +0200

    +add decimal format 1/2

--- app/src/main/java/cz/god/is/hony/spagetyapp/CalcActivity.java
+++ app/src/main/java/cz/god/is/hony/spagetyapp/CalcActivity.java
@@ -9,12 +9,18 @@ import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class CalcActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
     Spinner spinner;
     NumberPicker numberpicker;
     TextView textview1, textview, textview2;
     int spagety, omacka;
     double val;
+    String output;
+    double[] pole;
+    String[] sur;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -78,7 +84,8 @@ public class CalcActivity extends AppCompatActivity implements AdapterView.OnIte
      public void setIngedience(double newVal){
          if (spagety==1){
              double porce=newVal*100;
-             textview.setText("Špagety: "+porce+"g");
+             customFormat("###", porce);
+             textview.setText("Špagety: "+output+"g");
          }
          else if(spagety==2){
              textview.setText("Špagety: N/A");
@@ -95,19 +102,44 @@ public class CalcActivity extends AppCompatActivity implements AdapterView.OnIte
              double pepr=newVal*0.05;
              textview2.setText("Mléko: "+mleko+"ml"+"\nTavený sýr: "+taveny+"g"+"\nEidam: "+eidam+"g"+"\nSůl: "+sul+"g"+"\nKoření: "+koreni+"g"+"\nČerný mletý pepr: "+pepr+"q");
          }
-         else if(omacka==1){
-             double maso=newVal;
-             double rajcata=newVal*0.75;
-             double cukr=newVal*0.1;
-             double sul=newVal*0.05;
-             double koreni=newVal*0.1;
-             double pepr=newVal*0.05;
-             double paprika=newVal*0.15;
-             double oregano=newVal*0.1;
-             double cenek=newVal*0.2;
-             double cibule=newVal*0.3;
-             textview2.setText("Maso: "+maso+"g"+"\nPepř: "+pepr+"g"+"\nKoření na špagety: "+koreni+"g"+"\nSůl: "+sul+"g"+"\nRajčata: "+rajcata+"g"+"\nCukr: "+cukr+"g"+"\nMletá paprika: "+paprika+"g"+"\nOregano: "+oregano+"g"+"\nCibule"+cibule+"g"+"\nČesnek"+cenek+"g");
+         else {
+             if (omacka == 1) {
+                 double maso = newVal*100;
+                 double rajcata = newVal * 0.75;
+                 double cukr = newVal * 0.1;
+                 double sul = newVal * 0.05;
+                 double koreni = newVal * 0.1;
+                 double pepr = newVal * 0.05;
+                 double paprika = newVal * 0.15;
+                 double oregano = newVal * 0.1;
+                 double cenek = newVal * 0.2;
+                 double cibule = newVal * 0.3;
+                 pole = new double[]{maso, rajcata, cukr, sul, koreni, pepr, paprika, oregano, cenek, cibule};
+                 customFormat1("#.##");
+             }
          }
-         textview1.setText("suroviny na: " + newVal+"porcí.");
+         customFormat("###", newVal);
+         textview1.setText("suroviny na: " + output +" porcí.");
      }
+     public void customFormat(String pattern, double value ) {
+        DecimalFormat myFormatter = new DecimalFormat(pattern);
+        String output1 = myFormatter.format(value);
+        output = output1;
+
+    }
+    public void customFormat1(String pattern) {
+        int lenpole=pole.length;
+        sur = new String[lenpole];
+        for (int i = 0; i < lenpole; i++) {
+            DecimalFormat myFormatter = new DecimalFormat(pattern);
+            String output1 = myFormatter.format(pole[i]);
+            sur[i] = output1;
+        }
+        seTText();
+    }
+    public void seTText(){
+        textview2.setText("Maso: " + sur[0] + "g" + "\nPepř: " + sur[1] + "g" + "\nKoření na špagety: " + sur[2] + "g" + "\nSůl: " + sur[3] + "g" + "\nRajčata: " + sur[4] + "g" + "\nCukr: " + sur[5] + "g" + "\nMletá paprika: " + sur[6] + "g" + "\nOregano: " + sur[7] + "g" + "\nCibule: " + sur[8] + "g" + "\nČesnek: " + sur[9] + "g");
+    }
+
+
 }
